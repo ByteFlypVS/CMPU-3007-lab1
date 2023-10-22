697dd0404bed60c3be8d19a28481cacb21ba74d0
Shawn Frank Lloyd Lorenzo <123577300+ByteFlypVS@users.noreply.github.com>
2023-10-22 19:21:39 +0100

    More changes

--- src/routes/updateItem.js
+++ src/routes/updateItem.js
@@ -3,6 +3,7 @@ const db = require('../persistence');
 module.exports = async (req, res) => {
     await db.updateItem(req.params.id, {
         name: req.body.name,
+        // Change here
         completed: req.body.completed,
     });
     const item = await db.getItem(req.params.id);
